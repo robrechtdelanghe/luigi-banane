c6bb7b02166a832445b9c25422eca587106ea9f7
Robrecht De Langhe <robrecht.delanghe.ext@delijn.be>
2022-12-14 10:12:02 +0100

    Update button color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement>{}
 
 export default function Button({children, ...otherProps}: Props) {
   return <button
-    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-5 lg:mb-0 text-xl lg:text-sm min-w-[200px]"
+    className="bg-cyan-900 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded mb-5 lg:mb-0 text-xl lg:text-sm min-w-[200px]"
     {...otherProps}
   >
     {children}
